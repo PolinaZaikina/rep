758dd51b6a9b9f34fa057f48f2c4948be24a7c63
Polina.Zaikina <Polina.Zaikina@urfu.me>
2023-06-05 22:36:07 +0500

    вариант 10

---
+++ Task5.2/Task5.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task5._2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FitnessCenter fitnessCenter = new FitnessCenter();
+
+            fitnessCenter.Records.Add(new Record { ClientID = 1, Year = 2022, Month = 1, Duration = 5 });
+            fitnessCenter.Records.Add(new Record { ClientID = 1, Year = 2022, Month = 2, Duration = 3 });
+            fitnessCenter.Records.Add(new Record { ClientID = 2, Year = 2022, Month = 1, Duration = 7 });
+C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2\Microsoft.Build.Framework.dll            fitnessCenter.Records.Add(new Record { ClientID = 2, Year = 2022, Month = 2, Duration = 4 });
+            fitnessCenter.Records.Add(new Record { ClientID = 2, Year = 2022, Month = 3, Duration = 6 });
+            fitnessCenter.Records.Add(new Record { ClientID = 3, Year = 2022, Month = 1, Duration = 8 });
+
+            fitnessCenter.FindClientWithMaxDuration();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task5.2/task5_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5_1
+{
+    class Program
+    {
+        static int GetDifference(int[] array)
+        {
+            int halfLength = (int)Math.Ceiling(array.Length / 2.0);
+
+            int sumOdd = array
+                .Take(halfLength)
+                .Where(x => x % 2 != 0)
+                .Sum();
+            Console.WriteLine("Сумма нечетных чисел первой половины массива: " + sumOdd);
+
+            int sumEven = array
+                .Skip(halfLength - (array.Length % 2 == 0 ? 0 : 1))
+                .Where(x => x % 2 == 0)
+                .Sum();
+            Console.WriteLine("Сумма четных чисел второй половины массива: " + sumEven);
+
+            return sumOdd - sumEven;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа массива, разделяя их пробелом:");
+            int[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+            int difference = GetDifference(numbers);
+
+            Console.WriteLine("Разница: " + difference);
+            Console.ReadKey();
+        }
+    }
+}
